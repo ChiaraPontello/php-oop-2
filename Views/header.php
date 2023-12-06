983c9dacb231f9eae712d39cd2138203e3a4fe08
ChiaraPontello <chiarapontello1@gmail.com>
2023-12-06 15:24:45 +0100

    php-oop-1

---
+++ Views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>OOP-Shop</h1>
+            <ul>
+                <li><a href="index.php"></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
